5c8e7c4a7f330020dabf88600fd1927236d384b2
Bryce McMath <bryce.j.mcmath@gmail.com>
2017-10-26 21:16:17 -0700

    Added encoder feedback

--- DCDriver.ino
+++ DCDriver.ino
@@ -1,10 +1,10 @@
 #include <Encoder.h>
 
-Encoder myEnc(2, 3);
+Encoder myEnc(2, 3);				//declaring encoder pins (both interrupts)
 
-int potPin = A0;                //declaring pins
-int pwmA = 5;                 // "
-int pwmB = 6;                 // "
+int potPin = A0;              		//declaring pins
+int pwmA = 5;        		        // "
+int pwmB = 6;             		    // "
 
 
 void setup() {
@@ -25,14 +25,14 @@ void loop() {
       oldPosition = newPosition;
       Serial.println("Encoder pulse count is: " + String(newPosition));
 
-  int x = analogRead(potPin);         //read output from pot
+  int x = analogRead(potPin);         	//read output from pot
   int y;
   
   if(x >= 512) {
     
-  analogWrite(pwmB, 0);
+  analogWrite(pwmB, 0);					//sets alternate PWM pin to low
   y = map(x, 512, 1023, 0, 255);        //as the potentiometer turns clockwise from
-  analogWrite (pwmA, y);            //center, the clockwise motor output increases
+  analogWrite (pwmA, y);            	//center, the clockwise motor output increases
 
   //print input and output values to serial monitor 
   Serial.println("Potentiometer: " + String(x) + ", output A: " + String(y) + ", output B: 0.");
@@ -41,14 +41,14 @@ void loop() {
 
   else {
     
-  analogWrite(pwmA, 0);
-  y = map(x, 511, 0, 0, 255);         //as the potentiometer turns counter-clockwise from
-  analogWrite (pwmB, y);            //center, the counter-clockwise motor output increases
+  analogWrite(pwmA, 0);					//sets alternate PWM pin to low
+  y = map(x, 511, 0, 0, 255);         	//as the potentiometer turns counter-clockwise from
+  analogWrite (pwmB, y);            	//center, the counter-clockwise motor output increases
 
   //print input and output values to serial monitor
   Serial.println("Potentiometer: " + String(x) + ", output A: 0, output B: " + String(y) + ".");
   
   }
 
-  delay(1000);                //loop per second
+  delay(1000);                			//loop per second
 }
