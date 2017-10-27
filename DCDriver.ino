2a411e605d8308a539cb1db0411d06bc4f618b19
Bryce McMath <bryce.j.mcmath@gmail.com>
2017-10-26 20:36:00 -0700

    Added encoder feedback

--- DCDriver.ino
+++ DCDriver.ino
@@ -1,43 +1,54 @@
-/* Simple sketch to test new DC motor drivers with a potentiometer using the map function */
+#include <Encoder.h>
 
+Encoder myEnc(2, 3);
 
-int potPin = A0;								//declaring pins
-int pwmA = 5;									// "
-int pwmB = 6;									// "
+int potPin = A0;                //declaring pins
+int pwmA = 5;                 // "
+int pwmB = 6;                 // "
 
 
 void setup() {
-	
-	pinMode (potPin, INPUT);					//setting inputs and outputs
-	pinMode (potPin, INPUT);					// "
-	pinMode (potPin, INPUT);					// "
+  
+  pinMode (potPin, INPUT);          //setting inputs and outputs
+  pinMode (potPin, INPUT);          // "
+  pinMode (potPin, INPUT);          // "
 
-	Serial.begin(9600);
+  Serial.begin(9600);
 }
+
+long oldPosition  = -999;
 
 void loop() {
 
-	int x = analogRead(potPin);					//read output from pot
+    long newPosition = myEnc.read();
 
-	if(x >= 512) {
+      oldPosition = newPosition;
+      Serial.println("Encoder pulse count is: " + String(newPosition));
 
-	y = map(x, 512, 1023, 0, 255);				//as the potentiometer turns clockwise from
-	analogWrite (pwmA, y);						//center, the clockwise motor output increases
+  int x = analogRead(potPin);         //read output from pot
+  int y;
+  
+  if(x >= 512) {
+    
+  analogWrite(pwmB, 0);
+  y = map(x, 512, 1023, 0, 255);        //as the potentiometer turns clockwise from
+  analogWrite (pwmA, y);            //center, the clockwise motor output increases
 
-	//print input and output values to serial monitor 
-	Serial.println("Potentiometer: " + x + ", output A: " + y + ", output B: 0.");
+  //print input and output values to serial monitor 
+  Serial.println("Potentiometer: " + String(x) + ", output A: " + String(y) + ", output B: 0.");
 
-	}
+  }
 
-	else {
+  else {
+    
+  analogWrite(pwmA, 0);
+  y = map(x, 511, 0, 0, 255);         //as the potentiometer turns counter-clockwise from
+  analogWrite (pwmB, y);            //center, the counter-clockwise motor output increases
 
-	y = map(x, 511, 0, 0, 255);					//as the potentiometer turns counter-clockwise from
-	analogWrite (pwmB, y);						//center, the counter-clockwise motor output increases
+  //print input and output values to serial monitor
+  Serial.println("Potentiometer: " + String(x) + ", output A: 0, output B: " + String(y) + ".");
+  
+  }
 
-	//print input and output values to serial monitor
-	Serial.println("Potentiometer: " + x + ", output A: 0, output B: " + y + ".");
-	
-	}
-
-	delay(1000); 								//loop per second
+  delay(1000);                //loop per second
 }
